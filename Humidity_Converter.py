bb6d2ad538ce2c077cd8a5208caa30e194c907ab
Michael Pedruski <mpedruski@gmail.com>
2020-03-07 16:25:22 -0500

    Simplified function

--- Humidity_Converter.py
+++ Humidity_Converter.py
@@ -1,36 +1,22 @@
 import math
 
-def saturation(pressure, temperature):
-    ''' (float, int) -> float
-    Calculates the staturation vapour pressure of moist air for given conditions
+def rel_humid(RH_input, temperature_input, temperature_output):
+    ''' (int, int, int) -> float
+    Returns the relative humidity of air that would result from changing
+    the temperature from temperature_input to temperature_output and holding
+    the amount of water in the air constant.
     '''
-    sat = (1.0016 + 3.15*pressure*10**-6 - 0.074/pressure) * 6.112 * math.exp(17.62*temperature/(243.12+temperature))
-    return sat
-
-def mass(RH, pressure, temperature):
-    ''' (int, float, int) -> float
-    Returns the mass of water in a given volume of air under parameters supplied'''
-    mw = RH*saturation(pressure,temperature)*molar_mass_water/(ideal_gas_constant*(temperature+273.15))
-    return mw
-
-def rel_humid(mass, pressure, temperature):
-    '''(float, float, int) -> float
-    Returns the relative humidity of air based on mass of water in the air, the air pressure
-    and the air temperature'''
-    RH = mass*ideal_gas_constant*(temperature+273.15)/(saturation(pressure, temperature)*molar_mass_water)
-    return RH
-
-molar_mass_water = 18.01528
-ideal_gas_constant = 8.3136
+    RH_output = RH_input * (temperature_output+273.15)/(temperature_input+273.15) * math.exp(17.62*temperature_input/(243.12+temperature_input))/math.exp(17.62*temperature_output/(243.12+temperature_output))
+    return RH_output
 
 ### Get input from user on outdoor conditions
 
-pressure = float(input('What is the current air pressure outside? (kPa) \n'))
 ambient_temperature = int(input('What is the current temperature outside? (C)\n'))
 ambient_RH = int(input('What is the current relative humidity outside? (%)\n'))
 indoor_temperature = int(input('And what is the current temperature inside? (C)\n'))
 
-mass_of_water = mass(ambient_RH,pressure*10,ambient_temperature)
-comparable_humidity = rel_humid(mass_of_water,pressure*10,indoor_temperature)
+### Calculate and provide output on what outdoor humidity would yield at indoor temperatures
+
+comparable_humidity = rel_humid(ambient_RH,ambient_temperature,indoor_temperature)
 
 print('If you bring the air outside into the house at indoor temperatures, the indoor relative humidity would be {}.'.format(comparable_humidity))
